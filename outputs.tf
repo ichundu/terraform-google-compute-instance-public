939ebf462e4a3a2ccdf9e63951608f8ab265b157
Arlind Bardulla <echundu@gmail.com>
2019-04-21 15:30:46 +0200

    Add instances_tags output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "instances_self_links" {
   description = "Output self_link of created instances."
   value       = "${google_compute_instance.instances.*.self_link}"
 }
+
+output "instances_tags" {
+  description = "Output tags of created instances."
+  value       = "${google_compute_instance.instances.*.tags}"
+}
